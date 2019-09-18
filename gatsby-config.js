2e4b48ae3820507d2d6c547fbbdadfb97af25e93
John Purcell <john.purcell@gmail.com>
2019-09-18 00:26:00 -0400

    pizza placeholder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ambetterhealth.pizza`,
+    description: `Because you know you're going to get pizza.`,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(202, 24, 125)`,
       marginBottom: `1.45rem`,
     }}
   >
